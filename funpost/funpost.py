f8729568518e0926b301daf911cacb704baa909b
jej <64602039+jej-v@users.noreply.github.com>
2023-03-30 12:38:14 +0300

    Add files via upload

--- funpost/funpost.py
+++ funpost/funpost.py
@@ -17,51 +17,50 @@ class Misc(commands.Cog):
         self.bot = bot
         self.footer = "Coming from jej's spaghetti code 🍝"
 
-        # Advice
-        @checks.has_permissions(PermissionLevel.REGULAR)
-        @commands.command(name='advice')
-        async def advice(self, ctx):
+    # Advice
+    @checks.has_permissions(PermissionLevel.REGULAR)
+    @commands.command()
+    async def advice(self, ctx):
 
-            '''Have a random slip of advice~'''
-            
-            url = "https://api.adviceslip.com/advice"
+        '''Have a random slip of advice~'''
+        
+        url = "https://api.adviceslip.com/advice"
 
-            r = urlopen(url)
-            adv = json.loads(r.read().decode('utf-8'))
+        r = urlopen(url)
+        adv = json.loads(r.read().decode('utf-8'))
 
-            embed = discord.Embed(
-                title = f"Have a random slip of advice~",
-                description = f"{adv['slip']['advice']}",
-                colour = discord.Colour.random()
-            )
+        embed = discord.Embed(
+            title = f"Have a random slip of advice~",
+            description = f"{adv['slip']['advice']}",
+            colour = discord.Colour.random()
+        )
 
-            embed.set_thumbnail(url="https://upload-os-bbs.hoyolab.com/upload/2022/08/24/fbfc78ea104a8a3294edbb04352138fb_2018653294500640692.png")
-            
-            await ctx.respond(embed=embed)
+        embed.set_thumbnail(url="https://upload-os-bbs.hoyolab.com/upload/2022/08/24/fbfc78ea104a8a3294edbb04352138fb_2018653294500640692.png")
+        
+        await ctx.send(embed=embed)
 
-        # Gaydar
-        @checks.has_permissions(PermissionLevel.REGULAR)
-        @commands.command(self, name='gaydar')
-        async def gaydar(ctx, member: commands.MemberConverter):
+    # Gaydar
+    @checks.has_permissions(PermissionLevel.REGULAR)
+    @commands.command()
+    async def gaydar(self, ctx, member: commands.MemberConverter):
 
-            '''🌈?'''
+        '''🌈?'''
+        print(member)
+        # self rate
+        if member is None:
+            member = ctx.user
+        
+        num = random.randint(1, 10001)/100
 
-            # self rate
-            if member is None:
-                member = ctx.user
-            
-            num = random.randint(1, 10001)/100
-            await ctx.respond("Calculating......")
-
-            embed = discord.Embed(
-                title = f"The 🏳️‍🌈 has decided...",
-                description = f"{member.name} is **{num}%** gae.",
-                colour = discord.Colour.random()
-            )
-            
-            embed.set_footer(text="100% accuracy, source: me")
-            
-            await ctx.edit(content=None, embed=embed)
+        embed = discord.Embed(
+            title = f"The 🏳️‍🌈 has decided...",
+            description = f"{member} is **{num}%** gae.",
+            colour = discord.Colour.random()
+        )
+        
+        embed.set_footer(text="100% accuracy, source: me")
+        
+        await ctx.send(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(Misc(bot))
