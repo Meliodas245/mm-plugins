260bc9facd697000b780877da9579d98d8bc8f83
jej <64602039+jej-v@users.noreply.github.com>
2023-04-17 18:40:00 +0300

    Add files via upload

---
+++ createcmd/createcmd.py
@@ -0,0 +1,31 @@
+import discord
+import json
+from discord.ext import commands
+
+class Custom(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    # Set of custom commands:
+    custom_commands = {
+        '?airplanes' : 'https://media.discordapp.net/attachments/1086240031720603708/1087156186467532830/image.png?width=1177&height=654'
+    }
+
+    # Executing custom commands
+    @checks.has_permissions(PermissionLevel.REGULAR)
+    @commands.command()
+    async def create(self, ctx, cmd, url):
+        
+        custom_commands[f'?{cmd}'] = url
+    
+    @commands.Cog.listener()
+    async def on_message(message, *args):
+        cmd, *args = msg.content.split(' ')
+
+        if cmd in custom_commands:
+            await message.channel.send(custom_commands[cmd])
+        
+        else:
+            await bot.process_commands(message)
+
+async def setup(bot):
+    await bot.add_cog(Custom(bot))
