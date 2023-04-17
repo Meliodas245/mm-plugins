5594ee314cebb94a759841a2097ae66dc5579058
jej <64602039+jej-v@users.noreply.github.com>
2023-04-17 18:48:30 +0300

    Update createcmd.py

--- createcmd/createcmd.py
+++ createcmd/createcmd.py
@@ -1,6 +1,8 @@
 import discord
 import json
 from discord.ext import commands
+from core import checks
+from core.models import PermissionLevel
 
 class Custom(commands.Cog):
     def __init__(self, bot):
