be2182a0b58b77335bc112b45a66457424eb7f84
jej <64602039+jej-v@users.noreply.github.com>
2023-04-20 17:09:03 +0300

    Update verification.py

--- verification/verification.py
+++ verification/verification.py
@@ -7,11 +7,18 @@ class Reaction(commands.Cog):
 
     # carl you tried
     @commands.Cog.listener()
-    async def on_reaction_add(reaction, member):
-        if reaction.message.id == 1097762971373027348:
-            guild = member.guild
+    async def on_raw_reaction_add(payload):
+        message_id = payload.message_id
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g : g.id == guild_id, bot.guilds)
+
+        if message_id == 1097762971373027348:
             role = discord.utils.get(guild.roles, id=896300858277494784)
-            member.add_roles(role)
         
+        if role is not None:
+            member = payload.member
+            if member is not None:
+                await member.add_roles(role)
+
 async def setup(bot):
     await bot.add_cog(Reaction(bot))
