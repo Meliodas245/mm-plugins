10ab6922887c5e2e60e6a63573f939dc525d0e05
jej <64602039+jej-v@users.noreply.github.com>
2023-04-20 16:56:58 +0300

    Add files via upload

---
+++ verification/verification.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+class Reaction(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    # carl you tried
+    @commands.Cog.listener()
+    async def on_reaction_add(reaction, member):
+        if reaction.message.id == 902847613865459732:
+            guild = member.guild
+            role = discord.utils.get(guild.roles, id=896300858277494784)
+            member.add_roles(role)
+        
+async def setup(bot):
+    await bot.add_cog(Reaction(bot))
