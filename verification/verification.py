0793440f16b591f0ba1dbfe3a8f1ca64ef3a5429
jej <64602039+jej-v@users.noreply.github.com>
2023-04-24 15:22:19 +0300

    Update verification.py

--- verification/verification.py
+++ verification/verification.py
@@ -12,7 +12,7 @@ class Reaction(commands.Cog):
     async def on_raw_reaction_add(self, payload):
         message_id = payload.message_id
         # Get Message object from the Verification Channel
-        channel = self.bot.get_channel(1097760934090514523) # Channel ID
+        channel = await self.bot.get_channel(1097760934090514523) # Channel ID
         msg = await channel.fetch_message(1097762971373027348) # Message ID
         
         # Get Guild
@@ -20,8 +20,6 @@ class Reaction(commands.Cog):
         guild = discord.utils.find(lambda g : g.id == guild_id, self.bot.guilds)
         
         member = payload.member
-        role = None
-        
         if message_id == 1097762971373027348 and payload.emoji.name == '✅':
             # Get Butterflies Role
             role = discord.utils.get(guild.roles, id=896300858277494784)
@@ -31,7 +29,7 @@ class Reaction(commands.Cog):
         
         # Remove other types of reactions
         elif message_id == 1097762971373027348:
-            await msg.remove_reaction(payload.emoji, member)
+            await msg.remove_reaction(payload.emoji.name, member)
         
         # If role exists
         if role is not None:
@@ -44,7 +42,10 @@ class Reaction(commands.Cog):
     @commands.command()
     async def fixreaction(self, ctx):
         '''Fixes the reactions in verification channel'''
-        msg = self.bot.get_message(1097762971373027348)
+        # Get Message object from the Verification Channel
+        channel = await self.bot.get_channel(1097760934090514523) # Channel ID
+        msg = await channel.fetch_message(1097762971373027348) # Message ID
+
         await msg.add_reaction('✅')
         
         embed = discord.Embed(description = 'Reaction added!', colour = discord.Colour.from_rgb(0, 255, 0))
