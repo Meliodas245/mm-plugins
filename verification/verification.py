db4b2f8513421eaf2cd0e73a073c11de59db3a58
jej <64602039+jej-v@users.noreply.github.com>
2023-04-24 14:49:18 +0300

    Update verification.py

--- verification/verification.py
+++ verification/verification.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import commands
-'''
-Currently not installed, as long as carl is working kekw
-'''
+from core import checks
+from core.models import PermissionLevel
+
 class Reaction(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -18,10 +18,28 @@ class Reaction(commands.Cog):
             # Get Butterflies Role
             role = discord.utils.get(guild.roles, id=896300858277494784)
         
+        # Remove other types of reactions
+        elif message_id == 1097762971373027348:
+            await remove_reaction(payload.emoji.name, member)
+        
+        # If role exists
         if role is not None:
             member = payload.member
-            if member is not None:
+            # If member exists, Is not a bot and Doesn't have the Muted Role
+            if member is not None and not member.bot and member.get_role(902856057662083103) is None:
                 await member.add_roles(role)
 
+            # Remove the reaction for member
+            if not member.bot:
+                await remove_reaction('✅', member)
+
+    # In case all reactions get cleared
+    @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
+    @commands.command()
+    async def fixreaction():
+        '''Fixes the reactions in verification channel'''
+        msg = self.bot.get_message(1097762971373027348)
+        await msg.add_reaction('✅')
+        
 async def setup(bot):
     await bot.add_cog(Reaction(bot))
