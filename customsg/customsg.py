f86d32230c6fe1311d4bae6357180ab2453e4934
jej <64602039+jej-v@users.noreply.github.com>
2023-05-07 00:57:51 +0300

    Update customsg.py

--- customsg/customsg.py
+++ customsg/customsg.py
@@ -6,13 +6,16 @@ class CustomMsg(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
+    @commands.Cog.listener("on_message")
+    async def goofy(self, message):
 
         # Check if message contains the **Word**
         if 'masochist' in message.content.lower():
             await message.channel.send('seele mains promotes holy and pure actions!')
+            await self.bot.process_commands(message)
         elif 'gepard' in message.content.lower():
             await message.channel.send('you mean g*pard')
+            await self.bot.process_commands(message)
+            
 async def setup(bot):
     await bot.add_cog(CustomMsg(bot))
