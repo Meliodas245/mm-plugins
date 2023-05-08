9515d063685f180255f5d98e9d0f54524d5be4e8
jej <64602039+jej-v@users.noreply.github.com>
2023-05-08 21:25:37 +0300

    Update customsg.py

--- customsg/customsg.py
+++ customsg/customsg.py
@@ -13,9 +13,9 @@ class CustomMsg(commands.Cog):
         if 'masochist' in message.content.lower():
             await message.channel.send('seele mains promotes holy and pure actions!')
             await self.bot.process_commands(message)
-        #elif 'gepard' in message.content.lower():
-        #    await message.channel.send('you mean g*pard')
-        #    await self.bot.process_commands(message)
+        elif 'gepard' in message.content.lower():
+            await message.channel.send('you mean g*pard')
+            await self.bot.process_commands(message)
             
 async def setup(bot):
     await bot.add_cog(CustomMsg(bot))
