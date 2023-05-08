a9aaf17245b7e7335c8ac04a7b24f417c9b77e7c
jej <64602039+jej-v@users.noreply.github.com>
2023-05-08 22:46:47 +0300

    Update customsg.py

--- customsg/customsg.py
+++ customsg/customsg.py
@@ -13,7 +13,7 @@ class CustomMsg(commands.Cog):
         if 'masochist' in message.content.lower():
             await message.channel.send('seele mains promotes holy and pure actions!')
             await self.bot.process_commands(message)
-        elif 'gepard' in message.content.lower():
+        elif 'gepard' in discord.utils.remove_markdown(message.content.lower()):
             await message.channel.send('you mean g*pard')
             await self.bot.process_commands(message)
             
