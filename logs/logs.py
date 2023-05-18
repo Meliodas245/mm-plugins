141d080aa14c71bb6b765274d6c1e81061c0d6b6
jej <64602039+jej-v@users.noreply.github.com>
2023-05-18 22:24:36 +0300

    Add files via upload

---
+++ logs/logs.py
@@ -0,0 +1,25 @@
+import discord
+import os
+from discord.ext import commands
+from core import checks
+from core.models import PermissionLevel
+
+
+class Logs(commands.Cog):
+
+    @checks.has_permissions(PermissionLevel.MODERATOR)
+    @commands.command()
+    async def logs(self, ctx):
+        
+        for filename in os.listdir("./temp"):
+
+            if filename.endswith(".txt"):
+                file = filename
+        files = [ 
+        discord.File(f'temp/{file}')
+        ]
+
+        await ctx.reply(files=files)
+
+async def setup(bot):
+    await bot.add_cog(Logs(bot))
