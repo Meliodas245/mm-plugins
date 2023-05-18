8a6f38c751696bcc944c38db547e3b3e64642e46
blankdvth <contact@blankdvth.com>
2023-05-18 13:32:30 -0400

    Cleanup verification and welcome

--- customsg/customsg.py
+++ customsg/customsg.py
@@ -1,25 +1,26 @@
 import discord
 from discord.ext import commands
 
+
 # FUTURE 2DO: make a generalized version of this
 class CustomMsg(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @commands.Cog.listener("on_message")
-    async def goofy(self, message):
-
+    async def on_message(self, message: discord.Message):
         # Check if message contains the **Word**
         if 'masochist' in message.content.lower():
             await message.channel.send('seele mains promotes holy and pure actions!')
-            await self.bot.process_commands(message)
         elif 'gepard' in discord.utils.remove_markdown(message.content.lower()):
             await message.channel.send('you mean g*pard')
-            await self.bot.process_commands(message)
         # sorry not sorry -jej
-        elif 'french' in discord.utils.remove_markdown(message.content.lower()) or 'france' in discord.utils.remove_markdown(message.content.lower()):
+        elif 'french' in discord.utils.remove_markdown(message.content.lower()) or \
+                'france' in discord.utils.remove_markdown(message.content.lower()):
             sticker = await discord.Client.fetch_sticker(self.bot,1089453469661937704)
-            await message.channel.send(stickers = (sticker, sticker))
-            
+            await message.channel.send(stickers=(sticker, sticker))
+        await self.bot.process_commands(message)
+
+
 async def setup(bot):
     await bot.add_cog(CustomMsg(bot))
