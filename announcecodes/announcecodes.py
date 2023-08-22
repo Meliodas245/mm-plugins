f1ec35083421743006cada6406ebe79d039b314d
blankdvth <contact@blankdvth.com>
2023-08-21 17:15:17 -0700

    Add support for additional info in field name

--- announcecodes/announcecodes.py
+++ announcecodes/announcecodes.py
@@ -17,7 +17,14 @@ class AnnounceCodes(commands.Cog):
     @commands.command(aliases=["ac", "announcecode"])
     @checks.has_permissions(PermissionLevel.MODERATOR)
     async def announcecodes(self, ctx: commands.Context, *codes):
-        """Announce HSR gift code(s) quickly. Codes are separated by spaces after the command"""
+        """Announce HSR gift code(s) quickly.
+
+        Codes are separated by spaces after the command, and are automatically capitalized.
+
+        You can add additional info (e.g. what the code gives) by putting it after a colon after the code.
+        If the additional info includes a space, you NEED to surround the ENTIRE segment in double-quotes. For example:
+        `?announcecodes "FOOBAR1:60xStellar Jade, 10xCoins" FOOBAR2 FOOBAR3`
+        """
         codes: list[str]
         if len(codes) > 25:
             return await ctx.reply("You can only post up to 25 codes at once.")
@@ -29,7 +36,10 @@ class AnnounceCodes(commands.Cog):
             colour=0x7a7dfd
         )
         for num, code in enumerate(codes):
-            embed.add_field(name=f"Code {num + 1}", value=code.upper(), inline=False)
+            name = f"Code {num + 1}"
+            if ":" in code:
+                code, name = code.split(":", 1)
+            embed.add_field(name=name, value=code.upper(), inline=False)
 
         msg = await ctx.reply("Announcing...")
         await channel.send(content=f"<@&{CODES_ROLE_ID}>", embed=embed, view=discord.ui.View().add_item(
