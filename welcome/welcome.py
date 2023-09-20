de3176f8633bfc3efb278e16cecb6f66cb38fa24
jej <64602039+jej-v@users.noreply.github.com>
2023-09-20 23:55:08 +0300

    Update welcome.py

--- welcome/welcome.py
+++ welcome/welcome.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 
-CHANNEL_ID = 896803126542229534
+CHANNEL_ID = 1106794799199174686
 
 
 class Welcome(commands.Cog):
@@ -15,16 +15,16 @@ class Welcome(commands.Cog):
         channel = self.bot.get_channel(CHANNEL_ID)
         if channel is not None:
             embed = discord.Embed(
-                title=f"Welcome to Seele Mains, {member.name}!",
-                description=f"Please read <#1097760934090514523> to avoid any trouble, and <#898896680886370334> to familiarize yourself with the server.\n\n We wish you a pleasant stay; if you need help, DM <@902093847889313833> to start a Modmail ticket!",
-                colour=discord.Colour.from_rgb(195, 177, 225)
+                title=f"Welcome to Ruan Mei Mains, {member.name}!",
+                description=f"Please read <#1106786003777224804> to avoid any trouble, and <#1106794848117342280> to familiarize yourself with the server.\n\n We wish you a pleasant stay; if you need help, DM <@1148374427441049750> to start a Modmail ticket!",
+                colour=discord.Colour.from_rgb(84, 140, 140)
             )
 
             embed.set_thumbnail(url=member.display_avatar)
             embed.set_image(
-                url='https://media.discordapp.net/attachments/902836100261879818/1099532213990735952/ezgif.com-gif-maker_7.gif')
+                url='https://cdn.discordapp.com/attachments/1099432790229004439/1153915275591294987/Ruan-Mei-Honkai-Star-Rail.png')
             embed.set_footer(text=f'Thanks to you, we now have {guild.member_count} members!',
-                             icon_url='https://media.discordapp.net/attachments/896803697223401522/1087555998434213978/seele_love.png')
+                             icon_url='https://cdn.discordapp.com/attachments/1106792127268139119/1153168727701995620/ruanmei_flower.png')
 
             await channel.send(content=member.mention, embed=embed)
 
