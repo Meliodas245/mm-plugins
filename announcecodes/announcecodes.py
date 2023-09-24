10a3b02e96c4b93aaa7a484d572de40406c3d3c7
blankdvth <contact@blankdvth.com>
2023-09-24 14:43:41 -0400

    Move to redeem code link

--- announcecodes/announcecodes.py
+++ announcecodes/announcecodes.py
@@ -32,19 +32,22 @@ class AnnounceCodes(commands.Cog):
         channel = self.bot.get_channel(CHANNEL_ID)
         embed = discord.Embed(
             title=f"New HSR Gift Code{'s' if len(codes) > 1 else ''}!",
-            description="To copy a code on mobile, tap and hold the code itself.",
+            description="You can quickly redeem a code by pressing the respective button below. "
+                        "Alternatively, you can manually redeem by copying the codes in the embed. "
+                        "To copy a code on mobile, tap and hold the code itself.",
             colour=0x7a7dfd
         )
+        view = discord.ui.View()
         for num, code in enumerate(codes):
             name = f"Code {num + 1}"
             if ":" in code:
                 code, name = code.split(":", 1)
-            embed.add_field(name=name, value=code.upper(), inline=False)
+            code = code.upper()
+            embed.add_field(name=name, value=code, inline=False)
+            view.add_item(discord.ui.Button(label=f"Redeem {name}", style=discord.ButtonStyle.url, url=f"https://hsr.hoyoverse.com/gift?code={code}"))
 
         msg = await ctx.reply("Announcing...")
-        await channel.send(content=f"<@&{CODES_ROLE_ID}>", embed=embed, view=discord.ui.View().add_item(
-            discord.ui.Button(label="Redeem Page", style=discord.ButtonStyle.url,
-                              url="https://hsr.hoyoverse.com/gift")))
+        await channel.send(content=f"<@&{CODES_ROLE_ID}>", embed=embed, view=view)
         await msg.edit(content="Codes have been announced")
 
 
