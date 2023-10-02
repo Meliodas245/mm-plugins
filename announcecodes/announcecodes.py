3736500362ab3c931eeef7b6c5b9cebb4bcea90e
blankdvth <contact@blankdvth.com>
2023-10-01 23:25:45 -0400

    Attempt to detect invalid codes that would result in URL problem

--- announcecodes/announcecodes.py
+++ announcecodes/announcecodes.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import discord
 from discord.ext import commands
 
@@ -26,7 +28,7 @@ class AnnounceCodes(commands.Cog):
         If the additional info includes a space, you NEED to surround the ENTIRE segment in double-quotes. For example:
         `?announcecodes "FOOBAR1:60xStellar Jade, 10xCoins" FOOBAR2 FOOBAR3`
         """
-        codes: list[str]
+        codes: List[str]
         if len(codes) > 25:
             return await ctx.reply("You can only post up to 25 codes at once.")
 
@@ -43,10 +45,17 @@ class AnnounceCodes(commands.Cog):
             name = f"Code {num + 1}"
             if ":" in code:
                 code, name = code.split(":", 1)
+            if not code.isalnum():
+                return await ctx.reply(
+                    f"The code *`{code.replace('`', '[backtick]')}`* doesn't look right, "
+                    f"please double-check that your command formatting is correct!"
+                )
             code = code.upper()
             embed.add_field(name=name, value=code, inline=False)
-            view.add_item(discord.ui.Button(label=f"Redeem {name}", style=discord.ButtonStyle.url, url=f"https://hsr.hoyoverse.com/gift?code={code}"))
-
+            view.add_item(discord.ui.Button(
+                label=f"Redeem {name}", style=discord.ButtonStyle.url,
+                url=f"https://hsr.hoyoverse.com/gift?code={code}"
+            ))
         msg = await ctx.reply("Announcing...")
         await channel.send(content=f"<@&{CODES_ROLE_ID}>", embed=embed, view=view)
         await msg.edit(content="Codes have been announced")
