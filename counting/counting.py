742e01404863c1f88049bcdb5d7c4cb32a242501
blankdvth <contact@blankdvth.com>
2023-10-25 17:55:39 -0400

    Rewrite basic counting logic, prep for changes

--- counting/counting.py
+++ counting/counting.py
@@ -10,31 +10,47 @@ class Counting(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.last_number = 0
+        self.last_message = None
 
     @commands.Cog.listener("on_message")
     async def counting_on_message(self, message: discord.Message):
-        if message.channel.id == COUNTING_CHANNEL and message.author.id != self.bot.user.id:
-            if message.content.isdigit():
-                expected_number = self.last_number + 1
-                if int(message.content) == expected_number:
-                    await message.add_reaction('✅')
-                    self.last_number = expected_number
-                else:
-                    dumb = message.author.display_name
-                    await message.add_reaction('❌')
-                    embed = discord.Embed(
-                        title=f"{dumb} doesn't know how to count",
-                        colour=discord.Colour.red()
-                    )
-                    embed.add_field(name="Highest number reached", value=self.last_number)
-                    embed.add_field(name="The Count has been reset to 0", value="Good luck next time")
-                    embed.set_thumbnail(
-                        url="https://img-os-static.hoyolab.com/communityWeb/upload/19dacf2bf7dad6cea3b4a1d8d68045a0.png"
-                    )
-                    await message.channel.send(embed=embed)
-                    self.last_number = 0
-            else:
-                await message.channel.send("please refrain from typing in this channel")
+        if not message.author or not message.author.guild or message.author.bot:  # Irrelevant
+            return
+        if message.channel.id != COUNTING_CHANNEL:  # Not the counting channel
+            return
+
+        if message.content.strip().isdigit():  # Is a number
+            current_number = int(message.content.strip())
+            expected_number = self.last_number + 1
+
+            if current_number == expected_number:  # They can count!
+                self.last_number = current_number
+                self.last_message = message
+                return await message.add_reaction('✅')
+            else:  # They can't count :(
+                await message.add_reaction('❌')
+                embed = discord.Embed(
+                    title="That doesn't look right! Better luck next time :)",
+                    description=f"{message.author.mention} ruined the count at **{self.last_number:,d}**. Next number is **1**.",
+                    colour=discord.Colour.red()
+                )
+                embed.set_author(name=message.author.display_name, icon_url=message.author.display_avatar.url)
+                embed.set_thumbnail(
+                    url="https://img-os-static.hoyolab.com/communityWeb/upload/19dacf2bf7dad6cea3b4a1d8d68045a0.png"
+                )
+                embed.set_footer(
+                    text="*If this detection appears incorrect, please report it to the bot development team.*"
+                )
+                self.last_number = 0
+                self.last_message = await message.channel.send(content="0", embed=embed)  # "0" content allows for count detection on restart
+                return
+        else:  # Not a number
+            # We are resending the message as our own embed to allow for the restatement of the number (so it doesn't get lost)
+            embed = discord.Embed(description=message.content)
+            embed.set_author(name=f"{message.author.display_name} (`{message.author.id}`)", icon_url=message.author.display_avatar.url)
+            embed.set_footer(text=f"*The count is currently at: **`{self.last_number:,d}`***")
+            await message.delete()
+            return await message.channel.send(embed=embed)
 
 
 async def setup(bot):
