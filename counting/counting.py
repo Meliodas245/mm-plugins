5887f53fe4f45b16daca000d8ec1fb9facb13735
blankdvth <contact@blankdvth.com>
2023-10-25 19:01:23 -0400

    Disallow users from counting twice in a row

--- counting/counting.py
+++ counting/counting.py
@@ -8,7 +8,11 @@ DUPLICATE_GRACE = 0.75  # Time in seconds to be lenient to duplicate messages
 
 
 def set_embed_author(embed: discord.Embed, member: discord.Member):
-    """Sets the author field for an embed from a member object, following a predefined format."""
+    """Sets the author field for an embed from a member object, following a predefined format.
+
+    :param embed: Embed to set the author for
+    :param member: Member object you want to get the data from
+    """
     embed.set_author(name=f"{member.display_name} ({member.id})", icon_url=member.display_avatar.url)
 
 
@@ -20,6 +24,27 @@ class Counting(commands.Cog):
         self.last_number = 0
         self.last_message = None  # For use in detecting notable message edits & deletes
         self.lock = asyncio.Lock()  # To prevent dual-processing edge-cases
+
+    async def fail(self, title: str, message: discord.Message):
+        """Method to send a count-failed message with a customizable title.
+
+        :param title: Title to use for the count-failed embed
+        :param message: Message that resulted in the count-fail
+        """
+        await message.add_reaction('❌')
+        embed = discord.Embed(
+            title=title,
+            description=f"{message.author.mention} ruined the count at **{self.last_number:,d}**. Next number is **1**.\n\n"
+                        "*If this detection appears incorrect, please report it to the bot development team.*",
+            colour=discord.Colour.red()
+        )
+        embed.set_thumbnail(
+            url="https://img-os-static.hoyolab.com/communityWeb/upload/19dacf2bf7dad6cea3b4a1d8d68045a0.png"
+        )
+        set_embed_author(embed, message.author)
+        self.last_number = 0
+        self.last_message = await message.channel.send(content="0", embed=embed)  # "0" content allows for count detection on restart
+        return
 
     @commands.Cog.listener("on_message")
     async def counting_on_message(self, message: discord.Message):
@@ -36,37 +61,36 @@ class Counting(commands.Cog):
                 current_number = int(message.content.strip())
                 expected_number = self.last_number + 1
 
-                if current_number == expected_number:  # They can count!
-                    self.last_number = current_number
-                    self.last_message = message
-                    return await message.add_reaction('✅')
-                else:  # They can't count :(
-                    await message.add_reaction('❌')
-
+                if current_number != expected_number:  # They can't count :(
                     # Grace period for when people send the same number at the same time
                     if current_number == self.last_number and message.author.id != self.last_message.author.id and (
                             message.created_at - self.last_message.created_at
                     ).total_seconds() <= DUPLICATE_GRACE:
+                        await message.add_reaction('❌')
                         return await message.reply(embed=discord.Embed(
                             title="That doesn't look right, but I'll give you a chance...",
                             description=f"{message.author.mention} sent a duplicate number, but within the grace period. "
                                         f"The count is still at **{self.last_number:,d}**.",
                             colour=discord.Colour.yellow()
                         ))
+                    return await self.fail("That doesn't look right! Better luck next time :)", message)
+                elif message.author.id == self.last_message.author.id:  # They're trying to count by themselves!
+                    return await self.fail("You can't count twice in a row!", message)
+                elif self.last_message.author.id == self.bot.user.id and len(self.last_message.embeds) > 0:
+                    embed = self.last_message.embeds[0]
+                    if ("editing" in embed.description or "deleting" in embed.description
+                    ) and str(message.author.id) in embed.author.name:  # Not a edit/delete message resulting from them
+                        await message.reply(
+                            content="Don't try to edit or delete your messages to get around detections please.\n"
+                                    "If you're seeing this by pure coincidence, don't worry about it.",
+                            delete_after=10
+                        )
+                        return await self.fail("You can't count twice in a row!", message)
 
-                    embed = discord.Embed(
-                        title="That doesn't look right! Better luck next time :)",
-                        description=f"{message.author.mention} ruined the count at **{self.last_number:,d}**. Next number is **1**.\n\n"
-                                    "*If this detection appears incorrect, please report it to the bot development team.*",
-                        colour=discord.Colour.red()
-                    )
-                    embed.set_thumbnail(
-                        url="https://img-os-static.hoyolab.com/communityWeb/upload/19dacf2bf7dad6cea3b4a1d8d68045a0.png"
-                    )
-                    set_embed_author(embed, message.author)
-                    self.last_number = 0
-                    self.last_message = await message.channel.send(content="0", embed=embed)  # "0" content allows for count detection on restart
-                    return
+                # They can count!
+                self.last_number = current_number
+                self.last_message = message
+                return await message.add_reaction('✅')
             else:  # Not a number
                 # We are resending the message as our own embed to allow for the restatement of the number (so it doesn't get lost)
                 embed = discord.Embed(description=message.content, colour=discord.Colour.light_gray())
